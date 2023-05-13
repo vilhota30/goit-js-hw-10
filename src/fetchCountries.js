3fd5ad2f2bce1b466ffeea74ac0f4bb778889363
vilhota30 <111867115+vilhota30@users.noreply.github.com>
2023-05-13 20:20:21 +0300

    завантажує бібліотеки, створює новий файл, для запиту на сервер

---
+++ src/fetchCountries.js
@@ -0,0 +1,15 @@
+const BASE_URL_API = 'https://restcountries.com/v3.1/name/';
+const searchParams = new URLSearchParams({
+    fields: 'name, capital, population, flags, languages',
+});
+
+export const fetchCountries = (name) => {
+  return fetch(`${BASE_URL_API}${name}${searchParams}`)
+    .then(response => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        return response.json();
+      });
+
+};
