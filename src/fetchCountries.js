fcd35dee8625f2f45fdcf7ad3c0323081e6d08ad
vilhota30 <111867115+vilhota30@users.noreply.github.com>
2023-05-13 22:19:19 +0300

    виправляє помилки

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,12 +1,12 @@
 const BASE_URL_API = 'https://restcountries.com/v3.1/name/';
 const searchParams = new URLSearchParams({
-    fields: 'name, capital, population, flags, languages',
+    fields: 'name,capital,population,flags,languages,',
 });
 
 export const fetchCountries = (name) => {
-  return fetch(`${BASE_URL_API}${name}${searchParams}`)
+  return fetch(`${BASE_URL_API}${name}?${searchParams}`)
     .then(response => {
-        if (!response.ok) {
+        if (response.status === 404) {
           throw new Error(response.status);
         }
         return response.json();
--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const countrieInfoEl = document.querySelector('.country-info');
 const searchInput = document.querySelector('#search-box');
 const bodyEl = document.querySelector('body');
 
+bodyEl.style.backgroundImage = 'radial-gradient( circle 610px at 5.2% 51.6%,  rgba(3,4,90,1) 0%, rgba(2,3,45,1) 97.5% )';
 countrieList.style.visibility = 'hidden';
 countrieInfoEl.style.visibility = 'hidden';
 
@@ -40,32 +41,30 @@ if(!inputValue) {
  });
 }
 
-const createMarkupCountryInfo = data => {
-    data.reduce(
-       (acc, {flags: {svg}, name, capital, population, languages}) => {
-        console.log(languages);
-        languages = Object.values(languages).join(', ');
-        console.log(name);
+const createMarkupCountryInfo = data =>
+  data.reduce(
+    (acc, { flags: { svg }, name, capital, population, languages }) => {
+      console.log(languages);
+      languages = Object.values(languages).join(', ');
+      console.log(name);
+      return (
+        acc +
+        ` <img src="${svg}" alt="${name}" width="320" height="auto">
+            <p> ${name.official}</p>
+            <p>Capital: <span> ${capital}</span></p>
+            <p>Population: <span> ${population}</span></p>
+            <p>Languages: <span> ${languages}</span></p>`
+      );
+    },
+    ''
+  );
 
-        return (
-            acc +
-            `<img src="${svg}" alt="${name}" width="32" height="auto">
-            <p>${name.official}</p>
-            <p>Capital:<span>${capital}</span></p>
-            <p>Population:<span>${population}</span></p>
-            <p>Languages<span>${languages}</span>></p>
-            `
-        );
-       },
-    )
-}
-
-const generateMarkupCountryList = (data) => data.reduce((acc, {name: {official, common}, flags: {svg}}) => {
+const generateMarkupCountryList = (data) => data.reduce((acc, { name: {official, common}, flags: {svg} }) => {
     return (
         acc +
         `<li>
         <img src="${svg}" alt="${common}" width="70">
-        <span>4${official}</span>
+        <span>${official}</span>
         </li>`
     );
 }, '');
@@ -76,6 +75,12 @@ function renderCountries (result) {
         countrieList.innerHTML = '';
         countrieList.style.visibility = 'hidden';
         countrieInfoEl.style.visibility = 'visible';
+        countrieInfoEl.innerHTML = createMarkupCountryInfo(result);
+    }
+    if(result.length >= 2 && result.length <= 10) {
+        countrieInfoEl.innerHTML = '';
+        countrieInfoEl.style.visibility = 'hidden';
+        countrieList.style.visibility = 'visible';
         countrieList.innerHTML = generateMarkupCountryList(result);
     }
 }
